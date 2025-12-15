d43729c05efa8496f15a197be65604e4f87b0bba
ErosxJack <jackxsam07@gmail.com>
2025-12-15 23:46:35 +0530

    changes

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -6,40 +6,61 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = "server=turntable.proxy.rlwy.net;port=11062;database=railway;user=root;password=axxyOASrQKYMOAoVZrClutnzTIUmHjVg";
 
+// 🔹 REQUIRED FOR RENDER
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
+
+// 🔹 DATABASE CONNECTION (works for now)
+var connectionString =
+    "server=turntable.proxy.rlwy.net;port=11062;database=railway;user=root;password=axxyOASrQKYMOAoVZrClutnzTIUmHjVg";
+
+// 🔹 JWT KEY FROM ENVIRONMENT VARIABLE
+var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+if (string.IsNullOrEmpty(jwtKey))
+{
+    throw new Exception("JWT_KEY environment variable is not set");
+}
+
+// 🔹 SERVICES
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 builder.Services.AddScoped<IExpenseService, ExpenseService>();
 builder.Services.AddScoped<IBudgetService, BudgetService>();
 builder.Services.AddScoped<AuthService>();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+// 🔹 AUTHENTICATION
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey =
-         new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-    };
-});
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
 builder.Services.AddAuthorization();
 
+// 🔹 CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
 });
 
 var app = builder.Build();
 
+// 🔹 MIDDLEWARE
 app.UseCors("AllowAll");
 
 app.UseSwagger();
@@ -47,5 +68,7 @@ app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
-app.Run();
+
+app.Run(); 
